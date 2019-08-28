92d7f4f2a8754e733acb96739968fc182fa4bd6f
Michelle <michelle.gu@khoros.com>
2019-08-28 16:09:24 -0500

    get timeline button worksgit diffgit diff

--- src/js/twitter.js
+++ src/js/twitter.js
@@ -1,14 +1,16 @@
 // AJAX request for timeline
 function getTimeline() {
-  	var xhttp = new XMLHttpRequest();
-    xhttp.open('GET', 'http://localhost:8080/api/1.0/twitter/timeline', true);
-	xhttp.onreadystatechange = function() {
+  	var xhr = new XMLHttpRequest();
+    xhr.open('GET', 'http://127.0.0.1:8080/api/1.0/twitter/timeline', true);
+	xhr.onreadystatechange = function() {
 		if (this.readyState == 4 && this.status == 200) {
 			document.getElementById("timeline").innerHTML = this.responseText;
 		}
 	};
-	xhttp.onerror = function() {
+	xhr.onerror = function() {
     	console.log('Error making request to get timeline.');
   	};
-	xhttp.send();
+	xhr.send();
 }
+
+// http://html5rocks-cors.s3-website-us-east-1.amazonaws.com/index.html
