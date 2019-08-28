2bc7d127b372c9539283d4e3e43b934121e37461
Michelle <michelle.gu@khoros.com>
2019-08-28 14:26:45 -0500

    added path for js files

--- src/js/twitter.js
+++ src/js/twitter.js
@@ -1,11 +1,14 @@
-
+// AJAX request for timeline
 function getTimeline() {
-	var xhttp = new XMLHttpRequest();
+  	var xhttp = new XMLHttpRequest();
+    xhttp.open('GET', 'http://localhost:8080/api/1.0/twitter/timeline', true);
 	xhttp.onreadystatechange = function() {
 		if (this.readyState == 4 && this.status == 200) {
 			document.getElementById("timeline").innerHTML = this.responseText;
 		}
 	};
-	xhttp.open("GET", "http://localhost:8080/api/1.0/twitter/timeline", true);
+	xhttp.onerror = function() {
+    	console.log('Error making request to get timeline.');
+  	};
 	xhttp.send();
 }
