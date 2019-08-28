7be060789d1fe85d40f0d9d89ad85568ed8f46b6
Michelle <michelle.gu@khoros.com>
2019-08-28 17:48:02 -0500

    make blocks using js for each post

--- src/js/twitter.js
+++ src/js/twitter.js
@@ -4,7 +4,7 @@ function getTimeline() {
     xhr.open('GET', 'http://127.0.0.1:8080/api/1.0/twitter/timeline', true);
 	xhr.onreadystatechange = function() {
 		if (this.readyState == 4 && this.status == 200) {
-			document.getElementById("timeline").innerHTML = this.responseText;
+			buildTimeline(this.responseText);
 		}
 	};
 	xhr.onerror = function() {
@@ -13,4 +13,41 @@ function getTimeline() {
 	xhr.send();
 }
 
-// http://html5rocks-cors.s3-website-us-east-1.amazonaws.com/index.html
+function buildTimeline(responseText) {
+	var jsonTimeline = JSON.parse(responseText).timeline;
+	console.log(jsonTimeline);
+	var timelineDiv = document.getElementById("timeline");
+
+	for (i = 0; i < jsonTimeline.length; i++) {
+		post = jsonTimeline[i];
+
+		var postDiv = document.createElement("div");
+
+		var imageSpan = document.createElement("span");
+		var image = document.createElement("img");
+		image.src = post.user.profileImageUrl;
+
+		var messageSpan = document.createElement("span");
+		var message = document.createTextNode(new Date(post.createdAt) + ' ' + post.message);
+		imageSpan.appendChild(image);
+		messageSpan.appendChild(message);
+		postDiv.appendChild(imageSpan);
+		postDiv.appendChild(messageSpan);
+		if (i % 2 == 0) {
+			postDiv.style.backgroundColor = "gray";
+		} else {
+			postDiv.style.backgroundColor = "lightgray";
+		}
+
+		timelineDiv.appendChild(postDiv);
+
+	}
+}
+
+	// The timestamp should be converted to a human readable date time.
+	// For every other tweet, alternate the background color.
+	// Each tweet message should be clickable, opening a new tab with the tweet on twitter.
+	// Include an image of the user who made the tweet.
+	// Also ensure you use a span element in your code as well.
+
+
