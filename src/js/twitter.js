0af00aa80d15b0a001553b05c9c790c3a03e5804
Michelle <michelle.gu@khoros.com>
2019-08-29 09:58:29 -0500

    twitter js

--- src/js/twitter.js
+++ src/js/twitter.js
@@ -1,5 +1,6 @@
 // AJAX request for timeline
 function getTimeline() {
+	console.log("caling getTimeline")
   	var xhr = new XMLHttpRequest();
     xhr.open('GET', 'http://127.0.0.1:8080/api/1.0/twitter/timeline', true);
 	xhr.onreadystatechange = function() {
@@ -9,8 +10,9 @@ function getTimeline() {
 	};
 	xhr.onerror = function() {
     	console.log('Error making request to get timeline.');
+		document.getElementById("timeline").innerHTML = "Unable to retrieve timeline at this time. Check back later.";
   	};
 	xhr.send();
 }
 
-// http://html5rocks-cors.s3-website-us-east-1.amazonaws.com/index.html
+getTimeline();
