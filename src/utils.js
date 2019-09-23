817eab3b7ef09ac0d8b04cbbda19052516b033c4
Michelle <michelle.gu@khoros.com>
2019-09-23 16:11:12 -0500

    return a promise and then use the promise properly

--- src/utils.js
+++ src/utils.js
@@ -1,23 +1,15 @@
 
 // AJAX call to retrieve timeline
-export function fetchTimeline(getTimeline) {
-    let promise = new Promise((resolve, reject) => {
+export function fetchTimeline() {
+    return new Promise((resolve, reject) => {
         let xhr = new XMLHttpRequest();
         xhr.open('GET', 'http://127.0.0.1:8080/api/1.0/twitter/timeline', true);
         xhr.onreadystatechange = () => {
             if (xhr.readyState == XMLHttpRequest.DONE && xhr.status == 200) {
-                resolve(xhr.responseText);
+                resolve(JSON.parse(xhr.responseText).timeline);
             }
         };
         xhr.onerror = () => reject(new Error('Error making request to get timeline.'));
         xhr.send();
     });
-    const onResolved = (resolvedValue) => {
-        getTimeline(JSON.parse(resolvedValue).timeline);
-    };
-    const onRejected = (error) => {
-        console.log(error);
-        document.getElementById("timeline").innerHTML = "Unable to retrieve timeline at this time. Check back later.";
-    };
-    promise.then(onResolved, onRejected);
 }
