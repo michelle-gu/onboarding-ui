421b234bd1148924e8f91c68f8e4869979fffb80
Michelle <michelle.gu@khoros.com>
2019-09-23 15:48:04 -0500

    use promise syntax

--- src/utils.js
+++ src/utils.js
@@ -1,17 +1,24 @@
 
 // AJAX call to retrieve timeline
 export function fetchTimeline(getTimeline) {
-    let xhr = new XMLHttpRequest();
-    xhr.open('GET', 'http://127.0.0.1:8080/api/1.0/twitter/timeline', true);
-    xhr.onreadystatechange = () => {
-        if (xhr.readyState == XMLHttpRequest.DONE && xhr.status == 200) {
-            let jsonTimeline = JSON.parse(xhr.responseText).timeline;
-            getTimeline(jsonTimeline);
-        }
+    let promise = new Promise((resolve, reject) => {
+        let xhr = new XMLHttpRequest();
+        xhr.open('GET', 'http://127.0.0.1:8080/api/1.0/twitter/timeline', true);
+        xhr.onreadystatechange = () => {
+            if (xhr.readyState == XMLHttpRequest.DONE && xhr.status == 200) {
+                resolve(xhr.responseText);
+            }
+        };
+        xhr.onerror = () => reject(new Error('Error making request to get timeline.'));
+        xhr.send();
+
+    });
+    const onResolved = (resolvedValue) => {
+        getTimeline(JSON.parse(resolvedValue).timeline);
     };
-    xhr.onerror = () => {
-        console.log('Error making request to get timeline.');
+    const onRejected = (error) => {
+        console.log(error);
         document.getElementById("timeline").innerHTML = "Unable to retrieve timeline at this time. Check back later.";
     };
-    xhr.send();
+    promise.then(onResolved, onRejected);
 }
