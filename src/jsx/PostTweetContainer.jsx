00f6d3d9ff685776429bcf4f6c0fe5d6832b2ddc
Michelle <michelle.gu@khoros.com>
2019-10-01 13:31:51 -0500

    change status colors and combine setstates

--- src/jsx/PostTweetContainer.jsx
+++ src/jsx/PostTweetContainer.jsx
@@ -9,6 +9,7 @@ class PostTweetContainer extends Component {
             postStatus: '',
             postCharCount: 0,
             postMessage: '',
+            statusClass: '',
         };
 
         this.updatePostCharCount = this.updatePostCharCount.bind(this);
@@ -16,16 +17,18 @@ class PostTweetContainer extends Component {
     }
 
     updatePostCharCount(event) {
-        this.setState({ postMessage: event.target.value });
-        this.setState({ postCharCount: event.target.value.length });
+        this.setState({ postMessage: event.target.value, 
+                        postCharCount: event.target.value.length });
     }
 
     postTweet() {
         postTweet(this.state.postMessage).then(() => {
-            this.setState({ postStatus: "Success!" });
+            this.setState({ postStatus: "Success!",
+                            statusClass: "success" });
         }, (error) => {
             console.log(error);
-            this.setState({ postStatus: "Failed to post tweet. Try again later!"});
+            this.setState({ postStatus: "Failed to post tweet. Try again later!",
+                            statusClass: "error" });
         });
     }
 
@@ -33,13 +36,13 @@ class PostTweetContainer extends Component {
         return (                    
             <div id="post-tweet-container" className="timeline-container">
                 <div id="text-area-div">
-                    <textarea id="post-tweet-textarea" name="hello" onChange={this.updatePostCharCount} autoFocus maxLength={280} rows={10} placeholder="What's happening?"></textarea>
+                    <textarea id="post-tweet-textarea" onChange={this.updatePostCharCount} autoFocus maxLength={280} rows={10} placeholder="What's happening?"></textarea>
                     <div id="char-count-div">
                         <span>{this.state.postCharCount}/280</span>
                     </div>
                 </div>
                 <div id="post-status-bar">
-                    <span id="post-status">{this.state.postStatus}</span>
+                    <span id="post-status" className={this.state.statusClass}>{this.state.postStatus}</span>
                     <button className="button" disabled={this.state.postCharCount == 0} onClick={this.postTweet}>Tweet</button>
                 </div>
             </div>
